bf1f7cd73ebb0d597ffa0e8a17a81483faf3a49c
JamesHennessyStanford <jameshennessy@stanford.edu>
2017-05-29 14:06:00 -0700

    Delete Names.py

--- Names.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-db = pymysql.connect(host='10.0.0.140',user='root',passwd='password')
-cursor = db.cursor()
-query = ("SHOW DATABASES")
-cursor.execute(query)
-for r in cursor:
-    print r
